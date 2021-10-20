3048280bd0e9306837a0eb279f64ea3194def931
NearMaick <maick_a_s@msn.com>
2021-10-20 20:15:26 -0300

    chore(mobile): initial settings #3 fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Home } from './src/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
