4268958fec57f6e42fc724620b3ef244f8c6125b
NearMaick <maick_a_s@msn.com>
2021-10-20 20:48:34 -0300

    chore(mobile): svg transform

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+import LogoSvg from '../../assets/logo.svg';
+
+import { styles } from './styles';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <Text>Sair</Text>
+    </View>
+  );
+}
