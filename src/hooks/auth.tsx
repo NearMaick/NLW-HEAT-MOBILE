7d0fa7dae05f3b5faff72228865139625a8f48b1
NearMaick <maick_a_s@msn.com>
2021-10-22 13:03:16 -0300

    chore(mobile): auth Context #1

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState } from 'react';
+
+type User = {
+  id: string;
+  avatar_url: string;
+  name: string;
+  login: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  isSigningIn: boolean;
+  signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
+}
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  async function signIn() {}
+
+  async function signOut() {}
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      signOut,
+      user,
+      isSigningIn
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  return useContext(AuthContext);  
+}
+
+export { AuthProvider, useAuth }
