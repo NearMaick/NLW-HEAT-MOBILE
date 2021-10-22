8ffc8ae374fd8bc9bbbe045e57664b6959443ca7
NearMaick <maick_a_s@msn.com>
2021-10-22 16:32:30 -0300

    feat(mobile): listMessages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   ScrollView,
   View
 } from 'react-native';
-import { Message } from '../Message';
+import { api } from '../../services/api';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
@@ -17,16 +18,29 @@ const message = {
   }
 }
 
-export function MessageList(){
+export function MessageList() {
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(messagesResponse.data)
+    }
+
+    fetchMessages();
+  })
+
   return (
     <ScrollView 
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+
+      {currentMessages.map(message => (
+        <Message key={message.id} data={message} />
+      ))}
+      
     </ScrollView>
   );
 }
